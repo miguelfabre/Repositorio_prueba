98e4fb4ff369a2b516e369f2e6bb36d7dfd286e9
MIGUEL <miguel92grnd@gmail.com>
2015-02-02 05:38:00 +0100

    update

---
+++ pruebadriveiv/chef/solo1.rb
@@ -0,0 +1,6 @@
+# PATH TO CHEF debe ser la ruta relativa a la carpeta de chef (llamada "chef").
+  
+file_cache_path "/etc/chef"
+cookbook_path "/etc/chef/cookbooks"
+json_attribs "/etc/chef/node1.json"
+
